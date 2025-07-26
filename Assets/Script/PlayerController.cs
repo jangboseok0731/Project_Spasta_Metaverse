28c6f5caefb8d7d6dd888edb2b5f9151f9d2af7b
장보석 (Jang Bo Seok) <heeno7700@gmail.com>
2025-07-26 21:15:45 +0900

    Player기본동작 구현
    대각선으로 움직이면 빨라진다는 조금의 문제가 있다 내가 혼자 해결 할 수 없으니 과제 제출 전에 확인해 보고싶다

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,14 +5,61 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float moveSpeed = 4f;
+    public float jumpForce = 5f;
+
+    private Rigidbody2D rigidbody;
+
+    public Transform transform;
+    private bool isGrounded = false;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            moveX = -1f;
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            moveX = 1f;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            moveY = 1f;
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            moveY = -1f;
+
+            rigidbody.velocity = new Vector2(moveX * moveSpeed, moveY *moveSpeed);
+
+    }
+    void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck == null) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
