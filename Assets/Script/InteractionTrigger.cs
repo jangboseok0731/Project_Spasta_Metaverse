847f4106e70e1e81b937228f6cf5b366a3e9d34e
장보석 (Jang Bo Seok) <heeno7700@gmail.com>
2025-07-27 14:52:00 +0900

    새로운 타입의 이미지들 추가와 맵 구성
    바닥 타일을 꾸밀만한 이미지들,캐릭터들을 추가 하고 맵을 좀 더 꾸며 놓았다.
    문제가 없을 시 여기서 맵 디자인을 더 추가 하지는 않을 것 같다

--- Assets/Script/InteractionTrigger.cs
+++ Assets/Script/InteractionTrigger.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class InteractionTrigger : MonoBehaviour
 {
+    public GameObject InteractionInfo;
     // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            InteractionInfo.SetActive(true);
+        }
     }
 }
