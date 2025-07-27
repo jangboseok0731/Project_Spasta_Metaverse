f24a393ab2013b29ac71b3329e738b18d786ccb2
장보석 (Jang Bo Seok) <heeno7700@gmail.com>
2025-07-27 15:23:35 +0900

    NPC상호작용(Trigger)
    박스 콜라이더 트리거 추가해서 충돌감지가 되면 미리 만들어 놓은 UIText가 켜지게끔 만들어

--- Assets/Script/InteractionTrigger.cs
+++ Assets/Script/InteractionTrigger.cs
@@ -6,11 +6,21 @@ public class InteractionTrigger : MonoBehaviour
 {
     public GameObject InteractionInfo;
     // Start is called before the first frame update
-    private void OnTriggerEnter2D(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("여기냐");
         if (other.CompareTag("Player"))
         {
             InteractionInfo.SetActive(true);
+            Debug.Log("상호작용 가능~");
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            InteractionInfo.SetActive(false);
         }
     }
 }
