1fb0f6ed03ccee0c9e1ad57397e491fa8086a335
장보석 (Jang Bo Seok) <heeno7700@gmail.com>
2025-07-27 22:53:39 +0900

    재구성...다시시작
    다른 이미지 파일로 만들어 보려 시도 했지만 아직 설정방법을 잘 모르고 시간이 없어서 강의 방식대로 만들기로 했다.

--- Assets/Script/FlapyBird/CameraFollow.cs
+++ Assets/Script/FlapyBird/CameraFollow.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
+   public Transform target;
+   float offsetX;
+
     void Start()
     {
-        
+    if (target == null)
+    return;
+
+    offsetX = transform.position.x - target.position.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+        return;
+
+        Vector3 pos = transform.position;
+        pos.x = target.position.x + offsetX;
+        transform.position = pos;
     }
 }
+
+
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static implicit operator GameManager(GameManager_FlappyBird v)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        throw new NotImplementedException();
     }
 }
